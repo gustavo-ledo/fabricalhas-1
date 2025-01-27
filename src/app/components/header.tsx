bd38f52e17593d01c4d52ea607c8f96dba8b6439
gustavo-ledo <gustavoledodasilv@gmail.com>
2025-01-26 21:27:55 -0300

    links funcionais

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
           {/* Botão Fale Conosco e Toggle Menu Mobile */}
           <div className="flex items-center lg:order-2">
             <a
-              href="#"
+              href="https://api.whatsapp.com/send?phone=5541992340296"
               className="text-white bg-amber-500 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 flex items-center space-x-2 text-center"
             >
               <WhatsappIcon />
