9b5bfa611009bbfc45145ff8617269467eecea3a
gustavo-ledo <gustavoledodasilv@gmail.com>
2025-01-26 22:20:12 -0300

    ajuste whatsapp

--- src/app/components/whatsappbutton.tsx
+++ src/app/components/whatsappbutton.tsx
@@ -2,7 +2,7 @@ import WhatsappIcon from "./setIcons/whatsapp";
 
 export default function WhatsappButton() {
     return (
-        <div className="w-[80px] h-[80px] text-white cursor-pointer hover:bg-green-400 flex items-center justify-center rounded-full fixed bottom-10 right-10 z-50 bg-green-500 shadow-lg">
+        <div className="lg:w-[80px] lg:h-[80px] w-[45px] h-[45] text-white cursor-pointer hover:bg-green-400 flex items-center justify-center rounded-full fixed bottom-4 right-4 lg:bottom-10 lg:right-10 z-50 bg-green-500 shadow-lg">
             <a 
                 href="https://api.whatsapp.com/send?phone=5541992340296" 
                 target="_blank" 
