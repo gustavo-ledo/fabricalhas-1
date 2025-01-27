b483ca40ae891c22a83d31d7381378a426bbca7c
gustavo-ledo <gustavoledodasilv@gmail.com>
2025-01-26 21:03:39 -0300

    Imagem de produtos adicionadas

--- src/app/components/cardproduct.tsx
+++ src/app/components/cardproduct.tsx
@@ -1,5 +1,5 @@
 import WhatsappIcon from "./setIcons/whatsapp";
-
+import Image from "next/image";
 export default function CardProduct () {
     return (
         <section className="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-12">
@@ -14,15 +14,14 @@ export default function CardProduct () {
             <div className="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
                 <div className="h-56 w-full">
                 <a href="#">
-                    <img className="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="" />
-                    <img className="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
+                     <Image src="/produtos/1.png" className="h-60 w-full object-contain" alt="fabricalhasaraucaria" height={200} width={200} />
                 </a>
                 </div>
                 <div className="pt-6">
-                <a href="#" className="text-xl font-semibold leading-tight text-gray-900 hover:underline dark:text-white">Coifa de Aço Galvanizado</a>
+                <a href="#" className="text-2xl font-semibold leading-tight text-blue-900 hover:underline dark:text-white">Coifa de Aço Galvanizado</a>
                 </div>
                 <div className="pt-3">
-                <p className="text-md h-[200px] font-medium leading-tight text-gray-900 dark:text-white">Garanta ventilação eficiente e duradoura com as coifas em aço galvanizado da Fabricalhas. Projetadas para cozinhas e espaços comerciais, nossas coifas eliminam fumaça e odores indesejados, promovendo um ambiente seguro e saudável. Fabricadas com materiais de alta resistência e instaladas por profissionais experientes, as coifas da Fabricalhas são a solução ideal para quem busca qualidade e confiabilidade.</p>
+                <p className="text-md h-[200px] font-medium leading-tight text-blue-900 dark:text-white">Garanta ventilação eficiente e duradoura com as coifas em aço galvanizado da Fabricalhas. Projetadas para cozinhas e espaços comerciais, nossas coifas eliminam fumaça e odores indesejados, promovendo um ambiente seguro e saudável. Fabricadas com materiais de alta resistência e instaladas por profissionais experientes, as coifas da Fabricalhas são a solução ideal para quem busca qualidade e confiabilidade.</p>
                 </div>
                 <div className="w-full flex items-center justify-center">
                      <a href="#" className="text-white bg-amber-500 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center space-x-2 text-center justify-center mt-4 w-[200px]"><WhatsappIcon/><span>Fale conosco</span></a>
@@ -31,15 +30,30 @@ export default function CardProduct () {
             <div className="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
                 <div className="h-56 w-full">
                 <a href="#">
-                    <img className="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="" />
-                    <img className="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
+                    <Image src="/produtos/6.png" className="h-60 w-full object-contain" alt="fabricalhasaraucaria" height={200} width={200} />
                 </a>
                 </div>
                 <div className="pt-6">
-                <a href="#" className="text-xl font-semibold leading-tight text-gray-900 hover:underline dark:text-white">Dutos e Curvas em Aço Galvanizado</a>
+                <a href="#" className="text-2xl font-semibold leading-tight text-blue-900 hover:underline dark:text-white">Dutos e Curvas em Aço Galvanizado</a>
                 </div>
                 <div className="pt-3">
-                <p className="text-md font-medium h-[200px] leading-tight text-gray-900 dark:text-white">Melhore a ventilação do seu espaço com os dutos e curvas em aço galvanizado da Fabricalhas. Nossos produtos são fabricados com materiais robustos e instalados por especialistas, assegurando eficiência e durabilidade em ambientes residenciais ou comerciais. Oferecemos soluções confiáveis para criar um ambiente confortável e livre de impurezas.</p>
+                <p className="text-md font-medium h-[200px] leading-tight text-blue-900 dark:text-white">Melhore a ventilação do seu espaço com os dutos e curvas em aço galvanizado da Fabricalhas. Nossos produtos são fabricados com materiais robustos e instalados por especialistas, assegurando eficiência e durabilidade em ambientes residenciais ou comerciais. Oferecemos soluções confiáveis para criar um ambiente confortável e livre de impurezas.</p>
+                </div>
+                <div className="w-full flex items-center justify-center">
+                     <a href="#" className="text-white relative bottom-8 bg-amber-500 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center space-x-2 text-center justify-center mt-4 w-[200px]"><WhatsappIcon/><span>Fale conosco</span></a>
+                </div>
+            </div>
+            <div className="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+                <div className="h-56 w-full">
+                <a href="#">
+                <Image src="/produtos/2.png" className="h-60 w-full object-contain" alt="fabricalhasaraucaria" height={200} width={200} />
+                </a>
+                </div>
+                <div className="pt-6">
+                <a href="#" className="text-2xl font-semibold leading-tight text-blue-900 hover:underline dark:text-white">Exaustores Eólicos</a>
+                </div>
+                <div className="pt-3">
+                <p className="text-md h-[200px] font-medium leading-tight text-blue-900 dark:text-white">Escolha exaustores eólicos de alto desempenho com a Fabricalhas. Nossos produtos são ideais para ventilação sustentável em residências e empresas, garantindo a renovação constante do ar. Fabricados com materiais duráveis e instalados por profissionais qualificados, os exaustores eólicos promovem um ambiente saudável e confortável, combinando eficiência e sustentabilidade.</p>
                 </div>
                 <div className="w-full flex items-center justify-center">
                      <a href="#" className="text-white bg-amber-500 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center space-x-2 text-center justify-center mt-4 w-[200px]"><WhatsappIcon/><span>Fale conosco</span></a>
@@ -48,15 +62,14 @@ export default function CardProduct () {
             <div className="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
                 <div className="h-56 w-full">
                 <a href="#">
-                    <img className="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="" />
-                    <img className="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
+                <Image src="/produtos/3.png" className="h-60 w-full object-contain" alt="fabricalhasaraucaria" height={200} width={200} />
                 </a>
                 </div>
                 <div className="pt-6">
-                <a href="#" className="text-xl font-semibold leading-tight text-gray-900 hover:underline dark:text-white">Exaustores Eólicos</a>
+                <a href="#" className="text-2xl font-semibold leading-tight text-blue-900 hover:underline dark:text-white">Calhas Sob Medida</a>
                 </div>
                 <div className="pt-3">
-                <p className="text-md h-[200px] font-medium leading-tight text-gray-900 dark:text-white">Escolha exaustores eólicos de alto desempenho com a Fabricalhas. Nossos produtos são ideais para ventilação sustentável em residências e empresas, garantindo a renovação constante do ar. Fabricados com materiais duráveis e instalados por profissionais qualificados, os exaustores eólicos promovem um ambiente saudável e confortável, combinando eficiência e sustentabilidade.</p>
+                <p className="text-md h-[200px] font-medium leading-tight text-blue-900 dark:text-white">Proteja sua propriedade com calhas sob medida de alta qualidade da Fabricalhas. Desenvolvemos soluções personalizadas para se adequar perfeitamente às especificidades de sua construção, garantindo uma instalação precisa e eficiente. Nossas calhas são projetadas para resistir ao clima local, proporcionando durabilidade e funcionalidade para manter sua propriedade protegida contra danos causados pela água.</p>
                 </div>
                 <div className="w-full flex items-center justify-center">
                      <a href="#" className="text-white bg-amber-500 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center space-x-2 text-center justify-center mt-4 w-[200px]"><WhatsappIcon/><span>Fale conosco</span></a>
@@ -65,15 +78,14 @@ export default function CardProduct () {
             <div className="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
                 <div className="h-56 w-full">
                 <a href="#">
-                    <img className="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="" />
-                    <img className="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
+                    <Image src="/produtos/5.png" className="h-60 w-full object-contain" alt="fabricalhasaraucaria" height={200} width={200} />
                 </a>
                 </div>
                 <div className="pt-6">
-                <a href="#" className="text-xl font-semibold leading-tight text-gray-900 hover:underline dark:text-white">Calhas Sob Medida</a>
+                <a href="#" className="text-2xl font-semibold leading-tight text-blue-900 hover:underline dark:text-white">Rufos para Telhados</a>
                 </div>
                 <div className="pt-3">
-                <p className="text-md h-[200px] font-medium leading-tight text-gray-900 dark:text-white">Proteja sua propriedade com calhas sob medida de alta qualidade da Fabricalhas. Desenvolvemos soluções personalizadas para se adequar perfeitamente às especificidades de sua construção, garantindo uma instalação precisa e eficiente. Nossas calhas são projetadas para resistir ao clima local, proporcionando durabilidade e funcionalidade para manter sua propriedade protegida contra danos causados pela água.</p>
+                <p className="text-md h-[200px] font-medium leading-tight text-blue-900 dark:text-white">Assegure a proteção do seu telhado com os rufos de alta qualidade da Fabricalhas. Oferecemos soluções personalizadas que atendem às necessidades específicas de sua propriedade, garantindo segurança e durabilidade contra intempéries. Com materiais resistentes e instalação profissional, nossos rufos são a escolha ideal para proteger sua estrutura e valorizar seu imóvel.</p>
                 </div>
                 <div className="w-full flex items-center justify-center">
                      <a href="#" className="text-white bg-amber-500 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center space-x-2 text-center justify-center mt-4 w-[200px]"><WhatsappIcon/><span>Fale conosco</span></a>
@@ -82,32 +94,14 @@ export default function CardProduct () {
             <div className="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
                 <div className="h-56 w-full">
                 <a href="#">
-                    <img className="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="" />
-                    <img className="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
+                <Image src="/produtos/4.png" className="h-60 w-full object-contain" alt="fabricalhasaraucaria" height={200} width={200} />
                 </a>
                 </div>
                 <div className="pt-6">
-                <a href="#" className="text-xl font-semibold leading-tight text-gray-900 hover:underline dark:text-white">Rufos para Telhados</a>
+                <a href="#" className="text-2xl font-semibold leading-tight text-blue-900 hover:underline dark:text-white">Condutores de Calhas</a>
                 </div>
                 <div className="pt-3">
-                <p className="text-md h-[200px] font-medium leading-tight text-gray-900 dark:text-white">Assegure a proteção do seu telhado com os rufos de alta qualidade da Fabricalhas. Oferecemos soluções personalizadas que atendem às necessidades específicas de sua propriedade, garantindo segurança e durabilidade contra intempéries. Com materiais resistentes e instalação profissional, nossos rufos são a escolha ideal para proteger sua estrutura e valorizar seu imóvel.</p>
-                </div>
-                <div className="w-full flex items-center justify-center">
-                     <a href="#" className="text-white bg-amber-500 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center space-x-2 text-center justify-center mt-4 w-[200px]"><WhatsappIcon/><span>Fale conosco</span></a>
-                </div>
-            </div>
-            <div className="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
-                <div className="h-56 w-full">
-                <a href="#">
-                    <img className="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="" />
-                    <img className="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
-                </a>
-                </div>
-                <div className="pt-6">
-                <a href="#" className="text-xl font-semibold leading-tight text-gray-900 hover:underline dark:text-white">Condutores de Calhas</a>
-                </div>
-                <div className="pt-3">
-                <p className="text-md h-[200px] font-medium leading-tight text-gray-900 dark:text-white">Escolha condutores de calhas eficientes e duráveis com a Fabricalhas. Projetados para garantir a drenagem adequada da água da chuva, nossos produtos previnem danos e mantêm sua propriedade segura. Fabricados com materiais resistentes e instalados por uma equipe especializada, os condutores de calhas da Fabricalhas oferecem proteção confiável para sua casa ou empresa.</p>
+                <p className="text-md h-[200px] font-medium leading-tight text-blue-900 dark:text-white">Escolha condutores de calhas eficientes e duráveis com a Fabricalhas. Projetados para garantir a drenagem adequada da água da chuva, nossos produtos previnem danos e mantêm sua propriedade segura. Fabricados com materiais resistentes e instalados por uma equipe especializada, os condutores de calhas da Fabricalhas oferecem proteção confiável para sua casa ou empresa.</p>
                 </div>
                 <div className="w-full flex items-center justify-center">
                      <a href="#" className="text-white bg-amber-500 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center space-x-2 text-center justify-center mt-4 w-[200px]"><WhatsappIcon/><span>Fale conosco</span></a>
