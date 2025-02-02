3f8a5fb28540d24574a407d8eae17e16324441ad
gustavo-ledo <gustavoledodasilv@gmail.com>
2025-02-01 22:41:18 -0300

    portifolio adicionado

--- src/app/components/portfolio.tsx
+++ src/app/components/portfolio.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-
+import Link from "next/link";
 interface Props {
     cidade?: string;
 }
@@ -15,16 +15,34 @@ export default function Portfolio({cidade}: Props) {
                 Conheça o amplo portfólio da Fabricalhas em {cidade}, com produtos como calhas, rufos, coifas, curvas, dutos, perfis metálicos e exaustores eólicos. Fabricados com materiais de alta qualidade, nossos itens proporcionam proteção confiável contra intempéries. Com uma equipe especializada, desenvolvemos soluções sob medida para atender às demandas específicas de cada cliente. Confie na Fabricalhas para garantir a proteção e durabilidade da sua propriedade!
                 </p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 max-w-screen-xl gap-x-2 gap-y-3 auto-rows-[200px] mt-2">
-                <div className="min-h-[50px] min-w-[400px] rounded-lg bg-neutral-100 md:col-span-2"></div>
-                <div className="min-h-[50px] min-w-[400px] rounded-lg bg-neutral-100"></div>
-                <div className="min-h-[50px] min-w-[400px] rounded-lg bg-neutral-100 md:row-span-2"></div>
-                <div className="min-h-[50px] min-w-[400px] rounded-lg bg-neutral-100"></div>
-                <div className="min-h-[50px] min-w-[400px] rounded-lg bg-neutral-100"></div>
-                <div className="min-h-[50px] min-w-[400px] rounded-lg bg-neutral-100 md:col-span-2"></div>
-                <div className="min-h-[50px] min-w-[400px] rounded-lg bg-neutral-100 md:col-span-2"></div>
-                <div className="min-h-[50px] min-w-[400px] rounded-lg bg-neutral-100"></div>
+            <div className="grid grid-cols-1 md:grid-cols-3 max-w-screen-xl gap-x-2 gap-y-3 auto-rows-[400px] mt-2">
+                <div className="min-h-[150px] min-w-[400px] rounded-lg bg-neutral-100 md:col-span-2">
+                    <Image src="/portifolio/42.jpeg" alt="fabricalhasaraucaria" className="w-full h-full object-cover rounded-lg" height={1500} width={1500} />
+                </div>
+                <div className="min-h-[150px] min-w-[400px] rounded-lg bg-neutral-100">
+                    <Image src="/portifolio/2.jpeg" alt="fabricalhasaraucaria" className="w-full  h-full object-cover rounded-lg" height={1500} width={1500} />
+                </div>
+                <div className="min-h-[150px] min-w-[400px] rounded-lg bg-neutral-100 md:row-span-2">
+                    <Image src="/portifolio/8.jpeg" alt="fabricalhasaraucaria" className="w-full  h-full object-cover rounded-lg" height={1500} width={1500} />
+                </div>
+                <div className="min-h-[150px] min-w-[400px] rounded-lg bg-neutral-100">
+                    <Image src="/portifolio/13.jpeg" alt="fabricalhasaraucaria" className="w-full  h-full object-cover rounded-lg" height={1500} width={1500} />
+                </div>
+                <div className="min-h-[150px] min-w-[400px] rounded-lg bg-neutral-100">
+                    <Image src="/portifolio/21.jpeg" alt="fabricalhasaraucaria" className="w-full  h-full  object-cover rounded-lg" height={1500} width={1500} />
+                </div>
+                <div className="min-h-[150px] min-w-[400px] rounded-lg bg-neutral-100 md:col-span-2">
+                    <Image src="/portifolio/31.jpeg" alt="fabricalhasaraucaria" className="w-full h-full object-cover rounded-lg" height={1500} width={1500} />
+                </div>
+                <div className="min-h-[150px] min-w-[400px] rounded-lg bg-neutral-100 md:col-span-2">
+                    <Image src="/portifolio/34.jpeg" alt="fabricalhasaraucaria" className="w-full  h-full object-cover rounded-lg" height={1500} width={1500} />
+                </div>
+                <div className="min-h-[150px] min-w-[400px] rounded-lg bg-neutral-100">
+                    <Image src="/portifolio/49.jpeg" alt="fabricalhasaraucaria" className="w-full  h-full object-cover rounded-lg" height={1500} width={1500} />
+                </div>
             </div>
+                <a href="" className="p-2 rounded-2xl bg-blue-800 text-white">Ver mais dos nossos trabalhos</a>
+            
         </div>
     )
 }
